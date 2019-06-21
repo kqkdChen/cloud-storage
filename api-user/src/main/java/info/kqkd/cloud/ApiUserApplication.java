f7991ea51d758ac84c3df9aba0d3a31f391d7f06
kqkd <657424056@qq.com>
2019-06-21 18:16:29 +0800

    增加注解

--- api-user/src/main/java/info/kqkd/cloud/ApiUserApplication.java
+++ api-user/src/main/java/info/kqkd/cloud/ApiUserApplication.java
@@ -3,9 +3,11 @@ package info.kqkd.cloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(scanBasePackages = "info.kqkd.cloud.*")
 @MapperScan("info.kqkd.cloud.dao")
+@EnableTransactionManagement
 public class ApiUserApplication {
 
     public static void main(String[] args) {
